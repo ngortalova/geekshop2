9fb118443628bc03a495a9ddc6acddc8bd7b55ab
ngortalova <tesla@list.ru>
2022-02-03 19:12:17 +0300

    debug toolbar

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path
+from django.urls import path, re_path, include
 
 import mainapp.views as mainapp
+from geekshop import settings
 
 app_name = 'mainapp'
 
 urlpatterns = [
-   path('', mainapp.ProductsListView.as_view(), name='index'),
-   path('category/<int:pk>/', mainapp.ProductsListView.as_view(), name='category'),
-   path('product/<int:pk>/', mainapp.ProductDetailView.as_view(), name='product'),
+    path('', mainapp.ProductsListView.as_view(), name='index'),
+    path('category/<int:pk>/', mainapp.ProductsListView.as_view(), name='category'),
+    path('product/<int:pk>/', mainapp.ProductDetailView.as_view(), name='product'),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
